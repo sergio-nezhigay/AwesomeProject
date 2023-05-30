1f7ed174d4493c18e09a01d036f4b2dc0f5d8aaa
Sergio Nezhigay <abc1971h@gmail.com>
2023-05-30 15:51:18 +0300

    remove unused container

--- App.js
+++ App.js
@@ -11,38 +11,29 @@ const MainStack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <NavigationContainer>
-        <MainStack.Navigator initialRouteName="PostsScreen">
-          <MainStack.Screen
-            name="Registration"
-            component={RegistrationScreen}
-            options={{ headerShown: false }}
-          />
-          <MainStack.Screen
-            name="Login"
-            component={LoginScreen}
-            options={{ headerShown: false }}
-          />
-          <MainStack.Screen
-            name="Home"
-            component={HomeScreen}
-            options={{ headerShown: false }}
-          />
-          <MainStack.Screen
-            name="Posts"
-            component={PostsScreen}
-            options={{ headerShown: false }}
-          />
-        </MainStack.Navigator>
-      </NavigationContainer>
-    </View>
+    <NavigationContainer>
+      <MainStack.Navigator initialRouteName="PostsScreen">
+        <MainStack.Screen
+          name="Registration"
+          component={RegistrationScreen}
+          options={{ headerShown: false }}
+        />
+        <MainStack.Screen
+          name="Login"
+          component={LoginScreen}
+          options={{ headerShown: false }}
+        />
+        <MainStack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ headerShown: false }}
+        />
+        <MainStack.Screen
+          name="Posts"
+          component={PostsScreen}
+          options={{ headerShown: false }}
+        />
+      </MainStack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "red", // Set the background color to white
-  },
-});
